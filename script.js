b31c15ba89c4c54f8693a3896ab60d0edfd2d315
jeddelich@gmail.com <jeddelich@gmail.com>
2026-01-05 16:23:00 -0500

    link-animations-added

---
+++ script.js
@@ -0,0 +1,7 @@
+function addProfilePicture() {
+    document.querySelector(".personal-logo--effect").classList.add("profile-picture");
+}
+
+function removeProfilePicture() {
+    document.querySelector(".personal-logo--effect").classList.remove("profile-picture");
+}
